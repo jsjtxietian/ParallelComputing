c4d0c2f02a96f8882584995bef4c6e9488180912
jsjtxietian <jsjtxietian@outlook.com>
2024-03-10 20:31:41 +0800

    cancellable tcp & thread_pool

--- cs431/src/hello_server/tcp.rs
+++ cs431/src/hello_server/tcp.rs
@@ -39,7 +39,11 @@ impl CancellableTcpListener {
     pub fn cancel(&self) -> io::Result<()> {
         // Set the flag first and make a bogus connection to itself to wake up the listener blocked
         // in `accept`. Use `TcpListener::local_addr` and `TcpStream::connect`.
-        todo!()
+        self.is_canceled.store(true, Ordering::Release);
+        if let Ok(addr) = self.inner.local_addr() {
+            let _ = TcpStream::connect(addr);
+        }
+        Ok(())
     }
 
     /// Returns an iterator over the connections being received on this listener.  The returned
@@ -53,7 +57,26 @@ impl Iterator for Incoming<'_> {
     type Item = io::Result<TcpStream>;
     /// Returns None if the listener is `cancel()`led.
     fn next(&mut self) -> Option<Self::Item> {
+        if self.listener.is_canceled.load(Ordering::Acquire) {
+            return None;
+        }
+
         let stream = self.listener.inner.accept().map(|p| p.0);
-        todo!()
+        match stream {
+            Ok(_) => Some(stream),
+            Err(e) => {
+                if e.kind() == io::ErrorKind::WouldBlock || e.kind() == io::ErrorKind::Interrupted {
+                    // If the accept call is non-blocking or interrupted, we check the cancel flag again.
+                    if self.listener.is_canceled.load(Ordering::Acquire) {
+                        None // Listener was canceled during the operation.
+                    } else {
+                        // Continue trying to accept connections.
+                        self.next()
+                    }
+                } else {
+                    Some(Err(e))
+                }
+            }
+        }
     }
 }
--- cs431/src/hello_server/thread_pool.rs
+++ cs431/src/hello_server/thread_pool.rs
@@ -2,7 +2,7 @@
 
 // NOTE: Crossbeam channels are MPMC, which means that you don't need to wrap the receiver in
 // Arc<Mutex<..>>. Just clone the receiver and give it to each worker thread.
-use crossbeam_channel::{unbounded, Sender};
+use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 
@@ -20,7 +20,33 @@ impl Drop for Worker {
     ///
     /// NOTE: The thread is detached if not `join`ed explicitly.
     fn drop(&mut self) {
-        todo!()
+        if let Some(thread) = self.thread.take() {
+            thread.join().unwrap();
+        }
+    }
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Receiver<Job>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    (job.0)();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            _id: id,
+            thread: Some(thread),
+        }
     }
 }
 
@@ -33,14 +59,26 @@ struct ThreadPoolInner {
 }
 
 impl ThreadPoolInner {
+    fn new() -> Self {
+        Self {
+            job_count: Mutex::new(0),
+            empty_condvar: Condvar::new(),
+        }
+    }
     /// Increment the job count.
     fn start_job(&self) {
-        todo!()
+        let mut count = self.job_count.lock().unwrap();
+        *count += 1;
     }
 
     /// Decrement the job count.
     fn finish_job(&self) {
-        todo!()
+        let mut count = self.job_count.lock().unwrap();
+        *count -= 1;
+
+        if *count == 0 {
+            self.empty_condvar.notify_all();
+        }
     }
 
     /// Wait until the job count becomes 0.
@@ -48,7 +86,10 @@ impl ThreadPoolInner {
     /// NOTE: We can optimize this function by adding another field to `ThreadPoolInner`, but let's
     /// not care about that in this homework.
     fn wait_empty(&self) {
-        todo!()
+        let mut count = self.job_count.lock().unwrap();
+        while *count > 0 {
+            count = self.empty_condvar.wait(count).unwrap();
+        }
     }
 }
 
@@ -69,7 +110,19 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
-        todo!()
+        let (sender, receiver) = unbounded();
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver.clone()));
+        }
+
+        ThreadPool {
+            _workers: workers,
+            job_sender: Some(sender),
+            pool_inner: Arc::new(ThreadPoolInner::new()),
+        }
     }
 
     /// Execute a new job in the thread pool.
@@ -77,14 +130,21 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
-        todo!()
+        self.pool_inner.start_job();
+
+        let pool_inner_clone = self.pool_inner.clone();
+        let job = Box::new(move || {
+            f();
+            pool_inner_clone.finish_job();
+        });
+        self.job_sender.as_ref().unwrap().send(Job(job)).unwrap();
     }
 
     /// Block the current thread until all jobs in the pool have been executed.
     ///
     /// NOTE: This method has nothing to do with `JoinHandle::join`.
     pub fn join(&self) {
-        todo!()
+        self.pool_inner.wait_empty();
     }
 }
 
@@ -92,6 +152,6 @@ impl Drop for ThreadPool {
     /// When dropped, all worker threads' `JoinHandle` must be `join`ed. If the thread panicked,
     /// then this function should panic too.
     fn drop(&mut self) {
-        todo!()
+        drop(self.job_sender.take());
     }
 }
