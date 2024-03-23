a18a3a557b763e793e59876a9bd6882e6dc14f29
jsjtxietian <jsjtxietian@outlook.com>
2024-03-23 17:48:22 +0800

    easy version of fine grained linked list

--- cs431/src/list_set/fine_grained.rs
+++ cs431/src/list_set/fine_grained.rs
@@ -1,4 +1,6 @@
+use std::cmp::Ordering;
 use std::cmp::Ordering::*;
+use std::fmt::Display;
 use std::mem;
 use std::ptr;
 use std::sync::{Mutex, MutexGuard};
@@ -46,7 +48,22 @@ impl<T: Ord> Cursor<'_, T> {
     /// Moves the cursor to the position of key in the sorted list.
     /// Returns whether the value was found.
     fn find(&mut self, key: &T) -> bool {
-        todo!()
+        loop {
+            let current_ptr = *self.0;
+
+            if current_ptr.is_null() {
+                return false;
+            }
+
+            let current_node = unsafe { &*current_ptr };
+
+            match current_node.data.cmp(key) {
+                Ordering::Greater => return false,
+                Ordering::Less => self.0 = current_node.next.lock().unwrap(),
+
+                Ordering::Equal => return true,
+            }
+        }
     }
 }
 
@@ -61,21 +78,68 @@ impl<T> FineGrainedListSet<T> {
 
 impl<T: Ord> FineGrainedListSet<T> {
     fn find(&self, key: &T) -> (bool, Cursor<'_, T>) {
-        todo!()
+        let mut cursor = Cursor(self.iter().cursor);
+        (cursor.find(key), cursor)
     }
 }
 
-impl<T: Ord> ConcurrentSet<T> for FineGrainedListSet<T> {
+impl<T: Ord + Display> ConcurrentSet<T> for FineGrainedListSet<T> {
     fn contains(&self, key: &T) -> bool {
         self.find(key).0
     }
 
     fn insert(&self, key: T) -> bool {
-        todo!()
+        let mut prev_guard = self.head.lock().unwrap();
+        loop {
+            let current_ptr = *prev_guard;
+
+            if current_ptr.is_null() {
+                let node = Node::new(key, ptr::null_mut());
+                *prev_guard = node;
+                return true;
+            }
+
+            let current_node = unsafe { &*current_ptr };
+            match key.cmp(&current_node.data) {
+                Ordering::Greater => prev_guard = current_node.next.lock().unwrap(),
+                Ordering::Less => {
+                    let node = Node::new(key, current_ptr);
+                    *prev_guard = node;
+                    return true;
+                }
+                Ordering::Equal => return false,
+            }
+        }
     }
 
     fn remove(&self, key: &T) -> bool {
-        todo!()
+        let mut prev_guard = self.head.lock().unwrap();
+        loop {
+            let current_ptr = *prev_guard;
+
+            if current_ptr.is_null() {
+                return false;
+            }
+
+            let current_node = unsafe { &*current_ptr };
+            match key.cmp(&current_node.data) {
+                Ordering::Greater => {
+                    prev_guard = current_node.next.lock().unwrap();
+                }
+                Ordering::Less => {
+                    return false;
+                }
+                Ordering::Equal => {
+                    let mut next_guard = current_node.next.lock().unwrap();
+                    let new_next_node = *next_guard;
+                    *prev_guard = new_next_node;
+                    unsafe {
+                        let _ = Box::from_raw(current_ptr);
+                    }
+                    return true;
+                }
+            }
+        }
     }
 }
 
@@ -97,13 +161,24 @@ impl<'l, T> Iterator for Iter<'l, T> {
     type Item = &'l T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        let node_ptr = *(self.cursor);
+        if node_ptr.is_null() {
+            None
+        } else {
+            let result = Some(unsafe { &(*node_ptr).data });
+            self.cursor = unsafe { (*node_ptr).next.lock().unwrap() };
+            result
+        }
     }
 }
 
 impl<T> Drop for FineGrainedListSet<T> {
     fn drop(&mut self) {
-        todo!()
+        let mut current_ptr = *self.head.lock().unwrap();
+        while !current_ptr.is_null() {
+            let current = unsafe { Box::from_raw(current_ptr) };
+            current_ptr = *current.next.lock().unwrap();
+        }
     }
 }
 
